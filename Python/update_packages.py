7a3b6e49728fc45fafc8d07a41e307bb99ff29c1
Sandeep Sanyal <sandeep.sanyal@outlook.com>
2020-08-07 16:18:28 +0530

    python package update
    code that updates all python packages via pip

---
+++ Python/update_packages.py
@@ -0,0 +1,18 @@
+"""
+Script updates all python packages using package manager "pip"
+"""
+
+import pkg_resources
+from subprocess import call
+
+packages = [  # list all packages with available latest stable updates
+    dist.project_name
+    for dist in pkg_resources.working_set
+]
+
+call(  # update all packages in shell
+    "pip install --upgrade " + ' '.join(packages),
+    shell=True
+)
+
+del packages
