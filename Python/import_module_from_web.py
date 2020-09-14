e94f229ac7cd8a262b37be16130d5e8175a20c4e
Sandeep Sanyal <sandeep.sanyal@outlook.com>
2020-09-14 21:01:51 +0530

    function to import module from web

---
+++ Python/import_module_from_web.py
@@ -0,0 +1,37 @@
+# importing modules
+import os
+from importlib.util import module_from_spec
+import requests
+
+def import_cdn(
+  url: str,
+  name: str = None
+):
+    """
+    Imports a python module from web given it's URL
+
+    Parameters:
+    -----------
+    url: string
+        complete url to the location of the .py file
+    name: string (Optional)
+        Name of the python script
+        
+    Returns:
+    --------
+        An imported module
+
+    Usage:
+    module1 = import_cdn("url/path/to/a_file.py")
+    """
+    if not name:
+        name = os.path.basename(url).lower().rstrip('.py')
+ 
+    r = requests.get(url)
+    r.raise_for_status()
+ 
+    codeobj = compile(r.content, url, 'exec')
+    module = module_from_spec(name)
+    exec(codeobj, module.__dict__)
+    
+    return module
